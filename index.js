58441d49bbcff909acdf4a3791a498e5d48c694b
intositeme <intosite@gmail.com>
2016-03-03 13:44:19 +0800

    Update: Fixed Build directory

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = require('express')();
 var http = require('http').Server(app);
 
-app.use(express.static('bin'));
+app.use(express.static('build'));
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/bin/index.html');
+  res.sendFile(__dirname + '/build/index.html');
 });
 
 http.listen(3000, function(){
